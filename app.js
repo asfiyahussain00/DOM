61004e727bd462f8c8e991804f0a5dbbb7f65eac
Asfiya hussain <126233785+asfiyahussain00@users.noreply.github.com>
2024-11-18 17:05:59 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 document.getElementById("name").innerHTML="Hello DOM!" 
 document.getElementById("para").innerHTML="Asfiya Hussain!" 
 
+Digital Clock
+setInterval(() => {
+    document.getElementById('time').textContent = new Date().toLocaleTimeString();
+  }, 1000);
 
